8c825f108706fd773fd096648a188f6f418c4c28
AlainArnault <alain.arnault89@gmail.com>
2024-10-24 22:33:20 +0200

    ajout dernière section mais pbl background

---
+++ wp-content/themes/foce-child/template-parts/festival-nomination.php
@@ -0,0 +1,8 @@
+<section class="festival-nomination fade-in">
+    <div class="festival-nomination-col1">
+        <p>Fleurs d’oranger & chats errants est nominé aux Oscars Short Film Animated de 2022 !</p>
+    </div>
+    <div class="festival-nomination-col2">
+        <img src="/Koukaki/wp-content/themes/foce-child/assets/img/oscars-courts-metrages-francais-d-animation-2021.png" alt="nomination aux oscars 2021 des courts metrages">
+    </div>
+</section>
